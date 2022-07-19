3571efc908daa84387497e6e8ebdf10684fc83c3
AlexBosio95 <alex.bosio1995@gmail.com>
2022-07-19 15:06:16 +0200

    feat: basic structure

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <HeaderSite />
+    <MainSite />
+  </div>
+</template>
+
+<script>
+import HeaderSite from './components/HeaderSite.vue';
+import MainSite from './components/MainSite.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderSite,
+    MainSite,
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
