7643518e7dd9e82dafcaaf6858aa1322f527ae42
AlexBosio95 <alex.bosio1995@gmail.com>
2022-07-20 17:24:35 +0200

    fix: name searchGenre

--- src/components/SelectorSearch.vue
+++ src/components/SelectorSearch.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="input-group">
-        <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon" v-model="selGenre">
+        <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon" v-model="searchGenre">
             <option selected>All Genres...</option>
             <option value="Rock">Rock</option>
             <option value="Pop">Pop</option>
             <option value="Jazz">Jazz</option>
             <option value="Metal">Metal</option>
         </select>
-        <button class="btn btn-secondary" type="button" v-on="$emit('search', selGenre)">Search</button>
+        <button class="btn btn-secondary" type="button" v-on="$emit('search', searchGenre)">Search</button>
     </div>
     
 
@@ -17,7 +17,7 @@
 export default {
     data: function() {
         return{
-            selGenre: '',
+            searchGenre: '',
         }
     },
     created(){
